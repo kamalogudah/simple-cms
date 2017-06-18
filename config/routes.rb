894bd5d2a016f4838ec918a66b4d37da133856e6
kamalogudah <mcpaul2058@gmail.com>
2017-06-18 22:06:08 +0300

    Added Subjects controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  root 'subjects#index'
+  resources :subjects do
+    member do
+      get :delete
+    end 
+  end 
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
