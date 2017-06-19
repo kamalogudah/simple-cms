fd352ac62bbcbc62009f0bf3a904ca95cf08c1d5
kamalogudah <mcpaul2058@gmail.com>
2017-06-19 18:49:29 +0300

    Implemented the Create Action of Crud

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -8,9 +8,20 @@ class SubjectsController < ApplicationController
   end
 
   def new
+    @subject = Subject.new({:name => 'Default'})
   end
 
   def create 
+    #Instantiate a new object using form parameters 
+    @subject = Subject.new(subject_params)
+    #save the object
+    if @subject.save
+      #if save succeeds, redirect to the index action
+      redirect_to(subjects_path)
+    else
+      #if save fails, redisplay the form so user can fix problems
+      render('new')
+    end 
   end 
 
   def edit
@@ -25,4 +36,10 @@ class SubjectsController < ApplicationController
 
   def destroy
   end
+
+  private
+
+  def subject_params
+    params.require(:subject).permit(:name,:position, :visible)
+  end 
 end
