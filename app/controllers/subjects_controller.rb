4812469bbbfbf8464d027ca49bb6f68e6802eb9a
kamalogudah <mcpaul2058@gmail.com>
2017-06-19 19:01:10 +0300

    Implemented the Update action of Subjects Controller

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -12,11 +12,11 @@ class SubjectsController < ApplicationController
   end
 
   def create 
-    #Instantiate a new object using form parameters 
-    @subject = Subject.new(subject_params)
-    #save the object
-    if @subject.save
-      #if save succeeds, redirect to the index action
+    #Find an object using form parameters 
+    @subject = Subject.find(params[:id])
+    #update the object
+    if @subject.update_attributes(subject_params)
+      #if save succeeds, redirect to the show action
       redirect_to(subjects_path)
     else
       #if save fails, redisplay the form so user can fix problems
@@ -25,9 +25,20 @@ class SubjectsController < ApplicationController
   end 
 
   def edit
+    @subject = Subject.find(params[:id])
   end
 
-  def update 
+  def update
+    #Instantiate a new object using form parameters 
+    @subject = Subject.new(subject_params)
+    #update the object
+    if @subject.save
+      #if save succeeds, redirect to the index action
+      redirect_to(subject_path(@subject))
+    else
+      #if save fails, redisplay the form so user can fix problems
+      render('edit')
+    end  
   end 
 
 
