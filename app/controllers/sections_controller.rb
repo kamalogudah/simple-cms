93bdf04a8f41eed7d4d6ddd91542b4ae4e062acc
kamalogudah <mcpaul2058@gmail.com>
2017-06-20 19:27:14 +0300

    Added Pages and Subjects Controllers and their specific actions

---
+++ app/controllers/sections_controller.rb
@@ -0,0 +1,55 @@
+class SectionsController < ApplicationController
+  
+  def index
+    @sections = Section.sorted
+  end
+
+  def show
+    @section = Section.find(params[:id])
+  end
+
+  def new
+    @section = Section.new
+  end
+
+  def create
+     @section = Section.new(section_params)
+     if @section.save
+       flash[:notice] = "Section created successfully."
+       redirect_to(sections_path)
+     else
+       render('new')
+     end 
+  end
+
+  def edit
+    @section = Section.find(params[:id])
+  end
+
+  def update
+    @section = Section.find(params[:id])
+    if @section.update_attributes(section_params)
+      flash[:notice] = "Section successfully updated."
+      redirect_to(section_path(@section))
+    else
+      render('edit')
+    end
+  end
+
+  def delete
+    @section = Section.find(params[:id])
+  end
+
+  def destroy
+    @section = Section.find(params[:id])
+    @section.destroy
+    flash[:notice] = "Section Destroyed Successfully"
+    redirect_to(sections_path)
+  end
+
+  private 
+  
+  def section_params
+    params.require(:section).permit(:page_id, :name, :position, :visible, :content_type, :content)
+  end 
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,16 @@ Rails.application.routes.draw do
       get :delete
     end 
   end 
-
+  resources :pages do
+    member do
+      get :delete
+    end 
+  end 
+  resources :sections do
+    member do
+      get :delete
+    end 
+  end 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
