9e50d59e2e3d3548f67f8cb260ec6d046e9ff35c
kamalogudah <mcpaul2058@gmail.com>
2017-06-20 17:26:49 +0300

    Finished Subjects Controller

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -12,11 +12,12 @@ class SubjectsController < ApplicationController
   end
 
   def create 
-    #Find an object using form parameters 
-    @subject = Subject.find(params[:id])
-    #update the object
-    if @subject.update_attributes(subject_params)
+    #Instantiate a new object using form parameters 
+    @subject = Subject.new(subject_params)
+    #update the objectnew
+    if @subject.save
       #if save succeeds, redirect to the show action
+      flash[:notice] = "Subject Successfully created"
       redirect_to(subjects_path)
     else
       #if save fails, redisplay the form so user can fix problems
@@ -30,10 +31,11 @@ class SubjectsController < ApplicationController
 
   def update
     #Instantiate a new object using form parameters 
-    @subject = Subject.new(subject_params)
+    @subject = Subject.find(params[:id])
     #update the object
-    if @subject.save
+    if @subject.update_attributes(subject_params)
       #if save succeeds, redirect to the index action
+      flash[:notice] = "Subject Successfully updated"
       redirect_to(subject_path(@subject))
     else
       #if save fails, redisplay the form so user can fix problems
@@ -43,9 +45,14 @@ class SubjectsController < ApplicationController
 
 
   def delete
+    @subject = Subject.find(params[:id])
   end
 
   def destroy
+    @subject = Subject.find(params[:id])
+    @subject.destroy
+    flash[:notice] = "Subject '#{@subject.name}' Successfully destroyed"
+    redirect_to(subjects_path)
   end
 
   private
